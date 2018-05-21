0943b26eb96d73eed4568dc1ef81e91b8c93a94b
larry waldron <larry.waldron@gmail.com>
2018-05-21 19:39:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var recipes = {}
 
-function updaterecipeobject(object, key,value) {
+function updateObjectWithKeyAndValue(object, key,value) {
   return object.assign( {}, object, {key: value})
 
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+
+  object[key] = value
 }
